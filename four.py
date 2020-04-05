f49ad89779c11decb2ac46dfadeb163c35774143
KelseyCortez <kelscortez@gmail.com>
2020-04-05 17:13:19 -0400

    need help

--- four.py
+++ four.py
@@ -3,25 +3,10 @@
 width = int(input('Width? '))
 height = int(input('Height? '))
 
-h = 0
 
-while h < height:
+for i in range (width - 2):
+    print('*' * width)
+for i in range (height -2):
+    print('*' * height)
 
-    w = 0
-
-while w < width:
-
-    is_top = (h == 0)
-    is_bottom = (h == (height - 1))
-    is_left = (w == 0)
-    is_right = (w == (width -1))
-
-if is_top or is_bottom or is_left or is_right:
-    print('*', end='')
-
-else:
-    print('  ', end='')
-
-    h = h + 1
-    w = w + 1
-    
+print()
